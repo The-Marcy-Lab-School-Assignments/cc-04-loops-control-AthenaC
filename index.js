8bd701793f0e9448b74c29a459f326c299cc3cec
Athena <athenachang17@gmail.com>
2024-10-09 10:30:48 -0400

    finished and class notes

--- index.js
+++ index.js
@@ -14,38 +14,40 @@ const countEveryOdd = (num) => {
         };
     };
 };
-//countEveryOdd(10); // logs 1 3 5 7 9
+countEveryOdd(10); // logs 1 3 5 7 9
 
 // 3
-const isNegative = (i) => {
-    if (i < 0) {
-        return true;
-    } else {
-        return false;
-    };
-};
+const isNegative = i => i < 0 ? true : false;
+// {
+//     if (i < 0) {
+//         return true;
+//     } else {
+//         return false;
+//     };
+// };
 isNegative(3)         //returns false
 isNegative(-2)        //returns true
 isNegative(Math.PI)   //returns false
 
 // 4
-const betweenFiveAndTwenty = (i) => {
-    if (i >= 5 && i <= 20) {
-        return true;
-    } else {
-        return false;
-    };
-};
+const betweenFiveAndTwenty = i => i >= 5 && i <= 20 ? true : false;
+// {
+//     if (i >= 5 && i <= 20) {
+//         return true;
+//     } else {
+//         return false;
+//     };
+// };
 betweenFiveAndTwenty(12) // returns true
 betweenFiveAndTwenty(5) // returns true
 betweenFiveAndTwenty(100) // returns false
 
-// 5 not finished
+// 5 
 const sumOfThreeOrFive = () => {
     let sum = 0;
     for (let i = 1; i < 1000; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
-            sum += i;
+            sum += i; // reassignment of `sum`
         };
     };
     return sum;
@@ -54,12 +56,26 @@ sumOfThreeOrFive(); //returns 233168
 
 // 6
 const isAllLowerCase = (str) => {
-    if (str.toLowerCase("")) {
-        return false;
-    } else {
+    if (str.toLowerCase() === str) {
         return true;
+    } else {
+        return false;
     };
 };
-isAllLowerCase("hello") //returns true
+console.log(isAllLowerCase("hello")); //returns true
 isAllLowerCase("seven eleven") //returns true
-isAllLowerCase("Seven eleven has the best slushies") //returns false
+console.log(isAllLowerCase("Seven eleven has the best slushies")); //returns false
+
+// 7
+const countMultiplesOfFive = (arr) => {
+    let count = 0;
+    for (i = 0; i <= arr.length; i++) {
+        if (arr[i] % 5 == 0) {
+            count++;
+        }
+    }
+    return count;
+};
+countMultiplesOfFive([1,2,3,4,5,6,7,8,9,10]); // returns 2
+countMultiplesOfFive([15,23,35,45,67]) // returns 3
+countMultiplesOfFive([2,6,14]) // returns 0
